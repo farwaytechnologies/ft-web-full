72cd5bc57567b1fba3c51b41bcafe983b303f016
shaunsebastian <shaunsebastian4@gmail.com>
2025-09-29 15:57:18 +0530

    updated backed Links ports

--- frontend/src/Admin/AdminDashboard.jsx
+++ frontend/src/Admin/AdminDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import AdminSidebar from '../components/AdminSidebar';
+import AdminSidebar from '../components/AdminSidebar.jsx';
 import '../Styles/AdminStyle/AdminDashboard.css';
 
 const AdminDashboard = () => {
