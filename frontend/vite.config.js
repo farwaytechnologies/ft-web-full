e878a29181b7739593251ee05e8b0ceaebe3883b
shaunsebastian <shaunsebastian4@gmail.com>
2025-09-30 19:57:07 +0530

    updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
+    allowedHosts: [
+      'frontend-wpwr.onrender.com'
+    ]
+  }
 })
