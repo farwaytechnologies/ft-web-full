ddc7165889446ed61365464bc0203dd7a3fe695a
shaunsebastian <shaunsebastian4@gmail.com>
2025-09-30 23:13:12 +0530

    updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
     port: process.env.PORT || 3000,
     host: '0.0.0.0',
     allowedHosts: [
-      'frontend-wpwr.onrender.com'
+      'frontend-wpwr.onrender.com',
+      'farwaytechnologies.com'
     ]
   }
 })
