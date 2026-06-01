fc7beb3ffa4bbc1b402cede24641e16516cf52fe
shaunsebastian <shaunsebastian4@gmail.com>
2026-06-01 20:09:23 +0530

    microsoft clarity installed and config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { StudentProvider } from './context/StudentContext.jsx'
+import Clarity from '@microsoft/clarity';
+
+// Make sure to add your actual project id instead of "yourProjectId".
+const projectId = "x09r8zct44"
+
+Clarity.init(projectId);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
